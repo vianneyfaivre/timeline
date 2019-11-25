52a7743e34338205ee1c3137d8c361a7c2b881ef
Vianney FAIVRE <vianney.faivre@gmail.com>
2019-11-25 20:12:13 +1300

    added timeline type

--- src/Main.elm
+++ src/Main.elm
@@ -25,6 +25,15 @@ type Event
     | LongTime { name : String, startDate : String, endDate : String }
 
 
+type alias Timeline =
+    { startDate : String, endDate : String }
+
+
+type TimelineDate
+    = Start
+    | End
+
+
 type alias Model =
     { events : List Event }
 
@@ -32,12 +41,32 @@ type alias Model =
 init : Model
 init =
     Model
-        [ OneTime { name = "Event1", date = "17/04/1991" }
-        , LongTime { name = "Event2", startDate = "17/04/1991", endDate = "17/04/1992" }
+        [ LongTime { name = "API v1", startDate = "01/01/2020", endDate = "01/05/2020" }
+        , OneTime { name = "EOL", date = "01/05/2020" }
+        , LongTime { name = "API v2", startDate = "01/05/2020", endDate = "31/12/2020" }
         ]
 
 
+toTimeline : List Event -> Timeline
+toTimeline events =
+    { startDate = getTimelineDate events Start, endDate = getTimelineDate events End }
 
+
+getTimelineDate : List Event -> TimelineDate -> String
+getTimelineDate events timelineDate =
+    case timelineDate of
+        Start ->
+            -- events |> List.sortWith eventComparison |> List.maximum
+            "startTODO"
+
+        End ->
+            "endTODO"
+
+
+
+-- eventComparison : Event -> Event -> Order
+-- eventComparison e1 e2 =
+--     GT
 -- UPDATE
 
 
@@ -58,15 +87,22 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    -- do the business logic
+    let
+        timeline =
+            toTimeline model.events
+    in
+    -- draw the HTML
     div []
         [ drawInput ()
         , drawEvents model.events
+        , drawTimeline timeline
         ]
 
 
 drawInput : () -> Html Msg
 drawInput () =
-    textarea [] [ text "TODO: read input from here" ]
+    textarea [] [ text "TODO: When the submit button has been clicked, read events from this input, and update the view" ]
 
 
 drawEvents : List Event -> Html Msg
@@ -92,5 +128,6 @@ drawEventListItem event =
             li [] [ text data.name ]
 
 
-
--- li [] [ text event.name ]
+drawTimeline : Timeline -> Html Msg
+drawTimeline timeline =
+    timeline.startDate ++ " " ++ timeline.endDate |> text
